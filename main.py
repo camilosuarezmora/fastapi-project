2db422b4b20fb7968465cddf9f4adaeefe5bd5e2
camilosuarezmora <camilosuarezm2007@gmail.com>
2025-10-09 11:50:28 -0500

    Add initial FastAPI setup and comments for virtual environment instructions

---
+++ main.py
@@ -0,0 +1,6 @@
+# En un repositorio nuevo, primero se debe crear y activar un entorno virtual. 
+
+# pip install fastapi
+# es el comando que descarga <<fastapi>> desde "pip", y 
+from fastapi import FastAPI
+#importa un modulo llamado FastAPI desde el paquete fastapi.
