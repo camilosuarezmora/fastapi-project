5844a6d8da1ace1605e31b627947990ff677f85b
camilosuarezmora <camilosuarezm2007@gmail.com>
2025-10-22 15:59:47 -0500

    se inició la api

--- main.py
+++ main.py
@@ -4,3 +4,9 @@
 # es el comando que descarga <<fastapi>> desde "pip", y 
 from fastapi import FastAPI
 #importa un modulo llamado FastAPI desde el paquete fastapi.
+
+app = FastAPI() 
+
+@app.get("/")
+async def root():
+    return {"message": "Bienvenido a la API del curso"}
