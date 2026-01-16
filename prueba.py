557398313a840ca44e2c24fc4a0ce36bd2f2fa96
camilosuarezmora <camilosuarezm2007@gmail.com>
2026-01-15 23:17:44 -0500

    Refactor root function and add user listing logic with Pydantic model

--- prueba.py
+++ prueba.py
@@ -1,3 +1,5 @@
+"""
+#Lab de la función root
 def root():
     return {
         "message": "Bienvenido a la API del curso",
@@ -6,3 +8,52 @@ def root():
         }
     
 print(root())
+"""
+
+#Lab de lógica para listar usuarios por id
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+        id: int
+        nombre: str
+        apellido: str
+        telefono: int
+        direccion: str
+        correo: str
+
+
+ 
+users_list = [
+        User(
+                id = 1,
+                nombre = "camilo", 
+                apellido = "suarez", 
+                telefono = 3188477656, 
+                direccion="Cra 30 # 14 - 08", 
+                correo="kamsz.m0@gmail.com"
+        ),
+        User(
+                id = 2,
+                nombre = "laura", 
+                apellido = "mora", 
+                telefono = 3171375428, 
+                direccion="Algún lugar de lebrija", 
+                correo="valenmora@gmail.com"
+        ),
+        User(
+                id = 3,
+                nombre = "jhoan", 
+                apellido = "mora", 
+                telefono = 3178499515, 
+                direccion="Cra 30 # 14 - 09", 
+                correo="camilosuarezm2007@gmail.com"
+                )
+]
+
+for usuarios in users_list:
+    if usuarios.id == 2 :
+        print(usuarios)
+    # for usuario in usuarios:
+    #     if id == 2:        
+    #         print(usuario)
